4ca0dec4b82d549272217b585c597e8fcc3a1f71
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-07-15 14:07:29 +0200

    change scss location

--- imizon/src/Pages/User/Login.jsx
+++ imizon/src/Pages/User/Login.jsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col, Button, Form} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import FormSignIn from '../../Components/FormSignIn'
-import '../../Components/FormSingUp.scss';
 
 export default class login extends Component {
     render(){
