b141455b55ce6cc151175b5086b25a491ce1cbf6
J-Babin <39302514+J-Babin@users.noreply.github.com>
2021-07-16 16:28:42 -0500

    fini le front du product sheet

--- imizon/src/Components/ProductSheet.jsx
+++ imizon/src/Components/ProductSheet.jsx
@@ -1,82 +1,53 @@
-import { Container, Row, Col, Carousel } from "react-bootstrap";
-import { BiEuro } from 'react-icons/bi';
+import { Container, Row, Col } from "react-bootstrap";
 
+//image slider 
+import Image1 from '../Styles/Assets/testpc.jpeg';
+import Image2 from '../Styles/Assets/testpc2.jpeg';
+import ImageDesc from '../Styles/Assets/descLong.jpeg'
+
+// css
 import '../Styles/ProductSheet.scss';
-import Image1 from '../Styles/assets/testpc.jpeg';
-import Image2 from '../Styles/assets/testpc2.jpeg';
-export default function FormSignUp () {
+
+// Components
+import CarouselProduct from './ProductSheet/CarouselProduct'
+import InformationsProduct from './ProductSheet/InformationsProduct'
+import DescriptionLongProduct from './ProductSheet/DescriptionLongProduct'
+import ImgDescriptionLongProduct from './ProductSheet/ImageDescLong'
+
+
+export default function ProductSheet () {
     return(
         <Container fluid id="product_sheet">
             <Row className="style_card">
-                <Col>
-                    <div>premiere col</div>
-                    <Carousel>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src= {Image1}
-                                alt="First slide"
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src= {Image2}
-                                alt="second slide"
-                            />
-                        </Carousel.Item>
-                    </Carousel> 
-                </Col>
-
-                <Col>
-                    <Row> 
-                        <div> 
-                            <h5>PC Gamer - MSI MAG Infinite 10SA-1208FR 
-                                - Core i5-10400F - RAM 8Go - Stockage 1To HDD 
-                            </h5>
-                        </div> 
-                    </Row>
-                    <Row> 
-                        <div> 
-                            <p>
-                                Fourni sans carte graphique, ce PC Gamer Falcon équipé d’un processeur Intel Core i5 10600K vous permet de profiter dès maintenant de la puissance d’un PC de dernière génération tout en réutilisant votre carte graphique actuelle, en attendant la fin de la pénurie.
-                            </p> 
-                        </div> 
-                    </Row>
-                    <Row> 
-                        <div> 
-                            <p>1000 <BiEuro /></p>
-                        </div> 
-                    </Row>
-                    <Row> 
-                        <div> 
-                            En Stock / Nb en Stock 
-                        </div> 
-                    </Row>
-                    <Row> 
-                        <div> 
-                            btn Ajout panier 
-                        </div> 
-                    </Row>
-                </Col>
-
+                <CarouselProduct img1={Image1} img2={Image2}/>
+                <InformationsProduct 
+                    title="PC Gamer - MSI MAG Infinite 10SA-1208FR 
+                            - Core i5-10400F - RAM 8Go - Stockage 1To HDD " 
+                    description="Fourni sans carte graphique, ce PC 
+                        Gamer Falcon équipé d’un processeur 
+                        Intel Core i5 10600K vous permet de 
+                        profiter dès maintenant de la puissance 
+                        d’un PC de dernière génération tout en 
+                        réutilisant votre carte graphique actuelle,
+                            en attendant la fin de la pénurie."
+                    price="1 000"
+                    stock='12'/>
             </Row>
 
             <Row className="style_card">
                 <Col>
                     <Row> 
                         <Col id="title_product">
-                            <div> Title </div>
+                            <div> - Descriptif - </div>
+                            
                         </Col> 
                     </Row>
                     <Row>
-                        <Col id="description_product">
-                            <Row> <div> Description longue  </div> </Row>
-                        </Col>
-                        <Col id="image_product">
-                            <Row> <div> Image </div> </Row>
-                        </Col>
-    
+                        <DescriptionLongProduct/>
+                        <ImgDescriptionLongProduct
+                            id="img"
+                            img={ImageDesc}
+                        />
                     </Row>
                 </Col>
                 
---
+++ imizon/src/Components/ProductSheet/CarouselProduct.jsx
@@ -0,0 +1,26 @@
+import {Col, Carousel} from "react-bootstrap";
+
+
+ 
+export default function carousel (props) {
+    return(
+        <Col>
+            <Carousel  pause='hover' fade controls={false}>
+                <Carousel.Item className="carousel_item">
+                    <img
+                        className="d-block w-100"
+                        src= {props.img1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="carousel_item">
+                    <img
+                        className="d-block w-100"
+                        src= {props.img2}
+                        alt="second slide"
+                    />
+                </Carousel.Item>
+            </Carousel> 
+        </Col>
+    )
+}
