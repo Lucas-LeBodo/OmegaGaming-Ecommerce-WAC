64c0f9ee6b499a8629d94e6592833f207a5daddd
J-Babin <39302514+J-Babin@users.noreply.github.com>
2021-07-17 09:29:30 -0500

    ajout d'affichage conditionnel + btn ajout au panier + recuperation du Role utilisateur dans le localstorage et c'est info

--- imizon/src/Components/NavBar.jsx
+++ imizon/src/Components/NavBar.jsx
@@ -1,24 +1,62 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import '../Styles/NavBar.scss';
-
+import {decode as base64_decode} from 'base-64';
 function NavBar(props) {
-return (
-    <Fragment>
-        <div className="logbox">
-            <h2>Omega Gaming</h2>
-            <div className={'loglist'}>
-                <Link to={'/register'} >Register</Link>
-                <Link to={'/login'} >Login</Link>
+    let pathname = useLocation().pathname;
+    let loginPath = '';
+    let registerPath = '';
+    let logout ='';
+    let panier = '';
+    let jwt = localStorage.jwt
+    let role = '';
+    
+    const infoUser = (jwt) => {
+        let split = jwt.split('.');
+        let info = base64_decode(split[1]);
+        info = JSON.parse(info);
+        
+        var infoRole = info.roles
+        role = infoRole[0];
+    }
+
+    // conditional display btn login / register
+    if(pathname == "/login"){
+        loginPath = <Link to={'/register'} >Register</Link>
+    }
+    if(pathname == "/register"){
+        registerPath = <Link to={'/login'} >Login</Link>
+    }
+    if(pathname == "/" && !jwt){
+        registerPath = <Link to={'/login'} >Login</Link>
+        loginPath = <Link to={'/register'} >Register</Link>
+    }
+    if(jwt){
+        panier = <Link to={'#'} >Panier</Link>
+        logout = <Link to={'/'}  onClick={() => {localStorage.clear();}}>Logout</Link>
+        infoUser(jwt)
+    }
+
+    //console.log("valeur dans le localstorage ===> ", role)
+
+    return (
+        <Fragment>
+            <div className="logbox">
+                <h2>Omega Gaming</h2>
+                <div className={'loglist'}>
+                    {loginPath}
+                    {registerPath}
+                    {panier}
+                    {logout}
+                </div>
             </div>
-        </div>
-        <div className={'navbox'}>
-            <Link to={'/'} >Home</Link>
-            <Link to={'/'} >Page 2</Link>
-            <Link to={'/'} >Page 3</Link>
-        </div>
-    </Fragment>
-);
+            <div className={'navbox'}>
+                <Link to={'/'} >Home</Link>
+                <Link to={'/'} >Page 2</Link>
+                <Link to={'/'} >Page 3</Link>
+            </div>
+        </Fragment>
+    );
 }
 
 export default NavBar;
